5325d09312508e3308ee5b935cae9ba379acadb6
Pagend <pariy.illya@gmail.com>
2023-06-04 12:41:36 +0300

    add styles for all users

--- src/main/java/com/example/kursach/security/SecurityConfig.java
+++ src/main/java/com/example/kursach/security/SecurityConfig.java
@@ -21,6 +21,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests()
+                .requestMatchers("/css/*.css").permitAll()
                 .requestMatchers("/register").permitAll()
                 .anyRequest().authenticated()
                 .and()
